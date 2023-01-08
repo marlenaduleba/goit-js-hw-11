c84c853ef1448e12d26b50c42ee06742f4cd9433
Marlena Dulęba <marlena_duleba@o2.pl>
2023-01-08 13:59:54 +0100

    fetching function imported

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import Notiflix from 'notiflix';
+import { getPhotos } from './fetchPhotos';
+
+const form = document.querySelector("#search-form");
+const input = document.querySelector("#search-form__input");
+const button = document.querySelector("#search-form__btn");
+
+
+
+
+
