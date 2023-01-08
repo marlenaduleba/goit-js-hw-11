dda6c2be6869cf0dcd5757ddc6e9d13ef24aa151
Marlena Dulęba <marlena_duleba@o2.pl>
2023-01-08 21:29:50 +0100

    params corrected

--- src/js/fetchPhotos.js
+++ src/js/fetchPhotos.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
 
-
-
-export function getPhotos(name) {
-
-const baseUrl = `https://pixabay.com/api/`;
-const params = `?key=32655880-e7a49c73234a053b338665414&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`;
-
-    return axios.get(baseUrl + params);
+export async function getPhotos(searchValue) {
+  const baseUrl = `https://pixabay.com/api/`;
+  const searchParams = new URLSearchParams({
+    key: `32655880-e7a49c73234a053b338665414`,
+    q: searchValue,
+    image_type: `photo`,
+    orientation: `horizontal`,
+    safesearch: `true`,
+  });
+  const photos = await axios.get(`${baseUrl}?${searchParams}`);
+  return photos;
 }
